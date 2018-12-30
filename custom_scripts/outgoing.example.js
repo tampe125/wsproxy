ba7c3676cb43ae06beec927dc79ed41dfd000f84
Davide Tampellini <tampe125@gmail.com>
2018-12-30 16:24:56 +0100

    Working on making the repo public

---
+++ custom_scripts/outgoing.example.js
@@ -0,0 +1,13 @@
+// Handles and edits outgoing requests on the fly
+
+exports.editTextData = function (data) {
+  // Do your magic here
+
+  return data;
+};
+
+exports.editBinaryData = function (data) {
+  // Do your magic here
+
+  return data;
+};
